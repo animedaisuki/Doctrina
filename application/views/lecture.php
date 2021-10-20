b72b78b71cc469665d7e70c654419b5f4cab35ad
SummyTsang <s4594789@student.uq.edu.au>
2021-10-20 19:04:39 +0800

    front-end

--- application/views/lecture.php
+++ application/views/lecture.php
@@ -24,7 +24,6 @@
 
         <p style="padding: 20px 40px 20px 40px; line-height: 25px; text-align: left; font-size: 20px;">
             <?php echo $desc?>
-            desc
         </p>
     </div>
 
@@ -44,16 +43,11 @@
 
         <!--        <embed src="https://localhost/demo/uploads/files/47_Team_010.pdf" width="500" height="375">-->
 
-<!--            <video id="video" width="450" height="170" controls>-->
-<!--                <source src="https://localhost/demo/uploads/videos/UMAC.mp4" type="video/mp4">-->
-<!--            </video>-->
+        <!--        <video id="video" width="450" height="170" controls>-->
+        <!--            <source src="https://localhost/demo/uploads/videos/UMAC.mp4" type="video/mp4">-->
+        <!--        </video>-->
 
-    <a src="https://localhost/demo/uploads/videos/UMAC.mp4">
-
-
-    </a>
-
-
+    </div>
 
 </div>
 
@@ -75,16 +69,32 @@
         for(let i = 0; i<data.length; i++){
             const lec_video_row = document.createElement("div");
             lec_video_row.classList.add("lec_row");
-            lec_video_row.innerHTML = `
+
+            if (data === lec_videos_json_value){
+                lec_video_row.innerHTML = `
                 <div class="lec_row_play">
-                    <a href ="">
+                    <a href ="uploads/videos/${data[i].filename}">
                         ${icon}
                     </a>
                 </div>
                 <h4 class="lec_row_week">Week ${data[i].week}</h4>
                 <p class="lec_row_title">${data[i].filename}</p>
             `
-            lec_row_container.appendChild(lec_video_row);
+                lec_row_container.appendChild(lec_video_row);
+            }
+
+            if (data === lec_docs_json_value){
+                lec_video_row.innerHTML = `
+                <div class="lec_row_play">
+                    <a href ="uploads/files/${data[i].filename}">
+                        ${icon}
+                    </a>
+                </div>
+                <h4 class="lec_row_week">Week ${data[i].week}</h4>
+                <p class="lec_row_title">${data[i].filename}</p>
+            `
+                lec_row_container.appendChild(lec_video_row);
+            }
         }
     }
     render('<i class="fas fa-play-circle"></i>',lec_videos_json_value);
